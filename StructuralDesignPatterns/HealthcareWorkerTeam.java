5c117e7e149b06ffbbdcaa2e99f50d4b36fdf8e3
tyNAKAMOL <69255127+tyNAKAMOL@users.noreply.github.com>
2022-11-09 21:22:34 +0700

    Update HealthcareWorkerTeam.java

--- StructuralDesignPatterns/HealthcareWorkerTeam.java
+++ StructuralDesignPatterns/HealthcareWorkerTeam.java
@@ -10,27 +10,27 @@ public class HealthcareWorkerTeam implements HealthcareServiceable {
         members = new HashSet<>();
     }
 
-    public void addMember(HealthcareServiceable worker) {
-        members.add(worker);
+    public void addMember(HealthcareServiceable member) {
+        members.add(member);
     }
 
-    public void removeMember(HealthcareWorker worker) {
-        members.remove(worker);
+    public void removeMember(HealthcareWorker member) {
+        members.remove(member);
     }
 
     @Override
     public void service() {
-        for (HealthcareServiceable worker : members) {
-            worker.service();
+        for (HealthcareServiceable member : members) {
+            member.service();
         }
     }
 
     @Override
     public double getPrice() {
-        double total = 0;
-        for (HealthcareServiceable worker : members) {
-            total += worker.getPrice();
+        double sum_ = 0.0;
+        for (HealthcareServiceable member : members) {
+            sum_ += member.getPrice();
         }
-        return total;
+        return Sum_;
     }
 }
